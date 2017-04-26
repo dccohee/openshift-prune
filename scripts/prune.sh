86f90398200e4c33d69dc78605572848bc317cea
Garrett Potts <gpotts@radiantblue.com>
2017-04-26 09:33:37 -0400

    Added skip verify

--- scripts/prune.sh
+++ scripts/prune.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-echo oc login ${OPENSHIFT_URL} --username=${OPENSHIFT_USERNAME} --password=${OPENSHIFT_PASSWORD}
-oc login ${OPENSHIFT_URL} --username="${OPENSHIFT_USERNAME}" --password="${OPENSHIFT_PASSWORD}"
+oc login --insecure-skip-tls-verify --username="${OPENSHIFT_USERNAME}" --password="${OPENSHIFT_PASSWORD}" ${OPENSHIFT_URL} 
