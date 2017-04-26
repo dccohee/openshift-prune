4bee7c1cc36693a20421e9d5671df0feaf72cc7e
Garrett Potts <gpotts@radiantblue.com>
2017-04-26 09:42:26 -0400

    Added openshift-master as a node to juenkins

--- scripts/prune.sh
+++ scripts/prune.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 oc login --insecure-skip-tls-verify --username="${OPENSHIFT_USERNAME}" --password="${OPENSHIFT_PASSWORD}" ${OPENSHIFT_URL} 
+oc project o2
+oadm prune images --verify=true
