cae6501a069862bb0e406d0c82efac31ee7fe88b
Garrett Potts <gpotts@radiantblue.com>
2017-04-26 09:44:47 -0400

    Added prunes for the different projects

--- scripts/prune.sh
+++ scripts/prune.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 oc login --insecure-skip-tls-verify --username="${OPENSHIFT_USERNAME}" --password="${OPENSHIFT_PASSWORD}" ${OPENSHIFT_URL} 
 oc project o2
-oadm prune images --verify=true
+oadm prune images --confirm=true
+oadm prune builds --confirm=true
+oadm prune deployments --confirm=true
+
+oc project omar-rel
+oadm prune images --confirm=true
+oadm prune builds --confirm=true
+oadm prune deployments --confirm=true
+
+oc project omar-dev
+oadm prune images --confirm=true
+oadm prune builds --confirm=true
+oadm prune deployments --confirm=true
